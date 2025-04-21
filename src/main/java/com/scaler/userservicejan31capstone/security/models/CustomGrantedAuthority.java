734a5011f20253f0957ba7de50a435585b70a3ab
tuxsharx <pandeytushar359@gmail.com>
2025-04-21 21:20:55 +0530

    feat: implemented CustomUserDetail(oauth2)

---
+++ src/main/java/com/scaler/userservicejan31capstone/security/models/CustomGrantedAuthority.java
@@ -0,0 +1,18 @@
+package com.scaler.userservicejan31capstone.security.models;
+
+import com.scaler.userservicejan31capstone.models.Role;
+import org.springframework.security.core.GrantedAuthority;
+
+public class CustomGrantedAuthority implements GrantedAuthority
+{
+
+    String authority;
+    public CustomGrantedAuthority(Role role)
+    {
+        this.authority = role.getValue();
+    }
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
---
+++ src/main/java/com/scaler/userservicejan31capstone/security/models/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.scaler.userservicejan31capstone.security.models;
+
+import com.scaler.userservicejan31capstone.models.Role;
+import com.scaler.userservicejan31capstone.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails
+{
+    String username;
+    String password;
+    boolean accountNonExpired;
+    boolean accountNonLocked;
+    boolean credentialsNonExpired;
+    boolean enabled;
+    List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user)
+    {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+        this.authorities = new ArrayList<>();
+        for(Role role : user.getRoles())
+        {
+            authorities.add(new CustomGrantedAuthority(role));
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
